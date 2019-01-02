9b5cd9d80f78a21a19242cf24ac26592181d6d14
marcelotournier <dr.tournier@gmail.com>
2019-01-02 11:04:24 -0800

    Add files via upload

---
+++ mt_gcloud.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+############## start of script ##############
+
+echo "type your ip and press enter:" &&
+read YOUR_IP &&
+sudo adduser  --gecos "" user3 &&
+sudo usermod -aG sudo user3 &&
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config &&
+sudo service ssh restart &&
+read -p "
+
+## if you don't have SSH Keys - in client computer, run:
+
+ssh-keygen -f ~/.ssh/id_rsa &&
+ssh-copy-id -i ~/.ssh/id_rsa.pub user3@$YOUR_IP 
+
+## if you have a mac, install ssh-copy-id:
+
+curl -L https://raw.githubusercontent.com/beautifulcode/ssh-copy-id-for-OSX/master/install.sh &&
+sh
+
+<press any key to continue...>
+
+" &&
+
+# restore sshd_config to original and restart service
+sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config &&
+sudo service ssh restart &&
+exit
+
+############## end of script ############## 
